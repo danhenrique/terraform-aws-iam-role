9d6a6301116debc5b8d22e1d3574bb8eb2016bac
DanHenrique <danielhenrique.al@outlook.com>
2026-04-16 21:23:29 -0300

    fix variables

--- variables.tf
+++ variables.tf
@@ -25,13 +25,8 @@ variable "tags" {
   type        = map(string)
   default     = {}
   validation {
-<<<<<<< Updated upstream
-    condition = contains(keys(var.tags), "git_repository")
-    error_message = "The 'git_repository' tag is mandatory."
-=======
     condition     = contains(keys(var.tags), "Repository")
     error_message = "The 'Repository' tag is mandatory."
->>>>>>> Stashed changes
   }
 }
 
